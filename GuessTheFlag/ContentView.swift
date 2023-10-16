cf17d31efd191e290cd5f87a0e23d1782cb8ac6f
Scott Crowder <scrowder@gmail.com>
2023-10-16 12:18:18 -0500

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by SCOTT CROWDER on 10/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
